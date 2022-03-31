5315cf7e7f180040cfee8993cb9a4453dcd56e8f
Hongjia Liu <hongjia_liu@outlook.com>
2022-03-31 23:18:26 +0100

    Project complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { useState } from "react";
+import data from "./data";
+import List from "./List";
+
 function App() {
-	return <div>App Component</div>;
+	const [people, setPeople] = useState(data);
+
+	return (
+		<main>
+			<section className="container">
+				<h3>{people.length} birthdays today</h3>
+				<List people={people} />
+				<button
+					onClick={() => {
+						setPeople([]);
+					}}
+				>
+					Clear All
+				</button>
+			</section>
+		</main>
+	);
 }
 
 export default App;
--- src/List.js
+++ src/List.js
@@ -1,5 +1,19 @@
-const List = () => {
-	return <div>List Component</div>;
+const List = ({ people }) => {
+	return (
+		<>
+			{people.map(person => {
+				return (
+					<article key={person.id} className="person">
+						<img src={person.image} alt={person.name} />
+						<div>
+							<h4>{person.name}</h4>
+							<p>{person.age} years</p>
+						</div>
+					</article>
+				);
+			})}
+		</>
+	);
 };
 
 export default List;
